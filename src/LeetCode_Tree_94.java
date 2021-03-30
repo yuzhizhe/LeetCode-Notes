1afb2d5bfcca8f540efabf17dcac15fb571e43cf
yuzhizhe <yuzhizhe@yuzhizhe.cn>
2021-03-30 10:49:58 +0800

    树 94

--- src/LeetCode_Tree_94.java
+++ src/LeetCode_Tree_94.java
@@ -36,8 +36,8 @@ public class LeetCode_Tree_94 {
 		}
 		return list;
 	}
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		TreeNode root = new TreeNode(1);
 		root.left = new TreeNode(2, new TreeNode(3), new TreeNode(4));
 		root.right = new TreeNode(5, new TreeNode(6), new TreeNode(7));
