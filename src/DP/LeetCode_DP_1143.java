451794be4c2a1a0bfe27443f66e500d74bc85fc4
yuzhizhe <yuzhizhe@yuzhizhe.cn>
2021-08-11 18:16:49 +0800

    动态规划：1143

---
+++ src/DP/LeetCode_DP_1143.java
@@ -0,0 +1,34 @@
+package DP;
+public class LeetCode_DP_1143 {
+
+    /**
+     * 动态规划版本
+     * 时间复杂度：O(n*m)
+     * 空间复杂度：O(n*m)
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public static int longestCommonSubsequence(String text1, String text2) {
+        int length_1 = text1.length();
+        int length_2 = text2.length();
+        int[][] dp = new int[length_1 + 1][ length_2 + 1];
+        for (int i = 1; i <= length_1; i++){
+            char I = text1.charAt(i-1);
+            for (int j = 1; j <= length_2; j++){
+                char J = text2.charAt(j-1);
+                if (I == J){
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[length_1][length_2];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4,2,0,3,2,5};
+        System.out.println(longestCommonSubsequence("abc", "abc"));
+    }
+}
