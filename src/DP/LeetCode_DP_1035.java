f5140285d3a2d4861dd7fd17f5d0c2f8dd9507fe
yuzhizhe <yuzhizhe@yuzhizhe.cn>
2021-08-13 14:20:50 +0800

    动态规划： 1、修复1143优化版 2/1035

--- src/DP/LeetCode_DP_1035.java
+++ src/DP/LeetCode_DP_1035.java
@@ -1,23 +1,21 @@
 package DP;
-public class LeetCode_DP_1143 {
+public class LeetCode_DP_1035 {
 
     /**
      * 动态规划版本
      * 时间复杂度：O(n*m)
      * 空间复杂度：O(n*m)
-     * @param text1
-     * @param text2
+     * @param nums1
+     * @param nums2
      * @return
      */
-    public static int longestCommonSubsequence(String text1, String text2) {
-        int length_1 = text1.length();
-        int length_2 = text2.length();
-        int[][] dp = new int[length_1 + 1][ length_2 + 1];
+    public static int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int length_1 = nums1.length;
+        int length_2 = nums2.length;
+        int[][] dp = new int[length_1+1][length_2+1];
         for (int i = 1; i <= length_1; i++){
-            char I = text1.charAt(i-1);
             for (int j = 1; j <= length_2; j++){
-                char J = text2.charAt(j-1);
-                if (I == J){
+                if (nums1[i-1] == nums2[j-1]){
                     dp[i][j] = dp[i - 1][j - 1] + 1;
                 }else{
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
@@ -31,34 +29,34 @@ public class LeetCode_DP_1143 {
      * 动态规划版本--优化版
      * 时间复杂度：O(n*m)
      * 空间复杂度：O(n)
-     * @param text1
-     * @param text2
+     * @param nums1
+     * @param nums2
      * @return
      */
-    public static int longestCommonSubsequence1(String text1, String text2) {
-        int length_1 = text1.length();
-        int length_2 = text2.length();
-        int pre = 0;
+    public static int maxUncrossedLines1(int[] nums1, int[] nums2) {
+        int length_1 = nums1.length;
+        int length_2 = nums2.length;
         int[] dp = new int[length_2+1];
+        int pre = 0;
         for (int i = 1; i <= length_1; i++){
-            char I = text1.charAt(i-1);
             pre = 0;
             for (int j = 1; j <= length_2; j++){
-                char J = text2.charAt(j-1);
-                int tmp = dp[j];
-                if (I == J){
+                int temp = dp[j];
+                if (nums1[i-1] == nums2[j-1]){
                     dp[j] = pre + 1;
                 }else{
                     dp[j] = Math.max(dp[j-1], dp[j]);
                 }
-                pre = tmp;
+                pre = temp;
             }
         }
         return dp[length_2];
     }
 
     public static void main(String[] args) {
-        int[] nums = {4,2,0,3,2,5};
-        System.out.println(longestCommonSubsequence("abcba", "abcbcba"));
+        int[] S = {2,5,1,2,5};
+        int[] T = {10,5,2,1,5,2};
+        System.out.println(maxUncrossedLines(S, T));
+        System.out.println(maxUncrossedLines1(S, T));
     }
 }
